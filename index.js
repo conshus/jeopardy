42e5453be48e321e1d9780c80a66edd24b8407b1
Dwane <Dwane@hiphopandcode.com>
2017-04-07 18:07:37 -0400

    Base mode

--- index.js
+++ index.js
@@ -14,14 +14,15 @@ function Answer (categoryId, category, answerId, answer, question, points){
     if ((document.getElementById(inputId).value.toLowerCase().search(this.question.toLowerCase()) != -1) &&  (document.getElementById(inputId).value != "") ){
       console.log("correct");
       score += this.points;
+      this.showResult("correctResult");
     }else {
       console.log("not correct");
       score -= this.points;
+      this.showResult("incorrectResult");
     };
     let runningScore = document.querySelector("#score");
     runningScore.innerHTML = score;
     this.toggleAnswer();
-    getCategories();
   }
   this.showAnswer = function(event) {
     console.log("click", this.answerId);
@@ -39,6 +40,15 @@ function Answer (categoryId, category, answerId, answer, question, points){
     document.getElementById(this.categoryId).addEventListener('click', this.showAnswer.bind(this));
     let submitId = "submit"+this.answerId;
     document.getElementById(submitId).addEventListener('click', this.isCorrect.bind(this));
+  }
+  this.showResult =function (result){
+    document.getElementById("results").style.display = "block";
+    document.getElementById(result).style.display = "block";
+    setTimeout(function(){
+      document.getElementById("results").style.display = "none";
+      document.getElementById(result).style.display = "none";
+      getCategories();
+    }, 3000);
   }
   this.toggleAnswer = function (){
     let slide = document.getElementById(this.answerId).style;
@@ -72,8 +82,12 @@ function getCategories(){
     //.then(array => console.log(array))
 }
 function getStarted(){
-  document.getElementById("intro").style.display = "none";
-  getCategories();
+  if (document.getElementById("playerName").value != ""){
+    document.getElementById("intro").style.display = "none";
+    getCategories();
+    document.getElementById("player").style.display = "block";
+    document.getElementById("playerSign").textContent=document.getElementById("playerName").value;
+  }
 }
 document.getElementById("submitUserName").addEventListener('click', getStarted);
 //getStarted();
